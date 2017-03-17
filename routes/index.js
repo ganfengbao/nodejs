1f11d006ac7bebf356d6e60decd032da761f45e9
ganfengbao <ganfengabo@Gmail.com>
2017-03-17 18:10:56 +0800

    添加数据库信息

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var db = require("../model/db.js");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var sql = 'select * from sys_user';
+    db.find(sql,function (err,result) {
+        sql = "select * from cms_category a where a.parent_id = 1";
+        db.find(sql,function (err,result1) {
+            console.log(result1);
+            res.render('index', { result:  result1});
+        });
+    });
 });
 
 module.exports = router;
