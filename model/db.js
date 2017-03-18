8a574a937f6baa430dc3808a3e4208213c4183e4
ganfengbao <476263617@qq.com>
2017-03-18 19:34:16 +0800

    Update db.js

--- model/db.js
+++ model/db.js
@@ -2,12 +2,21 @@
  * Created by Administrator on 2017/3/16.
  */
 var mysql  = require('mysql');
+// 电脑密码
+// var pool  = mysql.createPool({
+//     connectionLimit : 10,
+//     host            : 'localhost',
+//     user            : 'root',
+//     password        : '1234',
+//     database        : 'jeesite'
+// });
+// 魔泊云
 var pool  = mysql.createPool({
     connectionLimit : 10,
-    host            : 'localhost',
-    user            : 'root',
-    password        : '1234',
-    database        : 'jeesite'
+    host            : '192.168.1.181',
+    user            : '06288107f4964',
+    password        : '7fae65869d754',
+    database        : 'd68a195b51f84'
 });
 
 exports.find = function (msql,callback) {
